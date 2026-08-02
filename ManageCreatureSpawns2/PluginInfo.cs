0ee0bd7d826519e0d6b50fe3fa22127c889080f4
GRex-work <91720800+GRex-work@users.noreply.github.com>
2026-08-01 20:42:21 -0400

    update version number

--- ManageCreatureSpawns2/PluginInfo.cs
+++ ManageCreatureSpawns2/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.arnoldrex.mods.subnautica.managecreaturespawns2";
         public const string PLUGIN_NAME = "ManageCreatureSpawns2";
-        public const string PLUGIN_VERSION = "2.0.0";
+        public const string PLUGIN_VERSION = "2.0.1";
     }
 }
